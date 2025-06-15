92fddc6ef4d7e8126de604d2980cd5a784130cdc
Saad Afzal <saaddk@gmail.com>
2025-06-15 12:48:40 +0200

    usefetch hook

---
+++ app/api/useFetch.ts
@@ -0,0 +1,44 @@
+// fetchMovies
+// fetchMovieDetails
+
+// useFetch(fetchMovies) Notice how this hook can be used with many different fetch functions
+
+import { useEffect, useState } from 'react';
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+	const [data, setData] = useState<T | null>(null); // default state is null
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string | null>(null);
+
+	const fetchData = async () => {
+		try {
+			setLoading(true);
+			setError(null); // Reset error state before fetching
+
+			const result = await fetchFunction();
+
+			setData(result);
+		} catch (err) {
+			// @ts-ignore
+			setError(err instanceof Error ? err : new Error('An error occurred'));
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const reset = () => {
+		setData(null);
+		setLoading(false);
+		setError(null);
+	};
+
+	useEffect(() => {
+		if (autoFetch) {
+			fetchData();
+		}
+	}, []);
+
+	return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
